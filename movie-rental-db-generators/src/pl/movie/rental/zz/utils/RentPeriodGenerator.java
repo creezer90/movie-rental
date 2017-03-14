667ae1170faa4d88187cf6c80695cb1d30e55ba6
Slawek <s.wodkowski@gmail.com>
2017-03-14 22:08:59 +0100

    variables names change

--- movie-rental-db-generators/src/pl/movie/rental/zz/utils/RentPeriodGenerator.java
+++ movie-rental-db-generators/src/pl/movie/rental/zz/utils/RentPeriodGenerator.java
@@ -20,30 +20,30 @@ public class RentPeriodGenerator {
 
 		int index = 0;
 
-		int movie_id = 1;
+		int movieId = 1;
 
-		long one_day = 86400000;
+		long oneDay = 86400000;
 
-		long date_from = FORMATTER.parse(dateFrom).getTime();
+		long dateFromLong = FORMATTER.parse(dateFrom).getTime();
 
-		long date_to = FORMATTER.parse(dateTo).getTime();
+		long dateToLong = FORMATTER.parse(dateTo).getTime();
 
-		long period_between_dates = (date_to - date_from);
+		long periodBetweenDates = (dateToLong - dateFromLong);
 
 		StringBuilder sb = new StringBuilder();
 
-		while (movie_id < 88) {
+		while (movieId < 88) {
 
-			long period = ((long) (Math.random() * 29) + 2) * one_day;
+			long period = ((long) (Math.random() * 29) + 2) * oneDay;
 
-			long start_date = date_from + ((long) (Math.random() * (period_between_dates - period)));
+			long startDate = dateFromLong + ((long) (Math.random() * (periodBetweenDates - period)));
 
-			long end_date = start_date + period;
+			long endDate = startDate + period;
 
 			sb.append("insert into rent_period (id, movie_id, start_date, end_date) values (").append(++index)
-					.append(", ").append(movie_id += (int) (Math.random() * 3) + 1).append(", '")
-					.append(FORMATTER.format(new Date(start_date))).append("', '")
-					.append(FORMATTER.format(new Date(end_date))).append("');\n");
+					.append(", ").append(movieId += (int) (Math.random() * 3) + 1).append(", '")
+					.append(FORMATTER.format(new Date(startDate))).append("', '")
+					.append(FORMATTER.format(new Date(endDate))).append("');\n");
 		}
 		return sb.toString();
 	}
